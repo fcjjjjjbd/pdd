f06d3f6be8e8a39302ba04a259f2ca474c243d22
转0为1,不1则0 <162014680+fcjjjjjbd@users.noreply.github.com>
2025-08-01 08:26:53 +0800

    Add App.vue and main.js files

---
+++ main.js
@@ -0,0 +1,32 @@
+import App from "./App";
+import uviewPlus from "@/uni_modules/uview-plus";
+import * as Pinia from "pinia";
+import piniasistedstate from "pinia-plugin-persistedstate";
+
+// #ifndef VUE3
+import Vue from "vue";
+import "./uni.promisify.adaptor";
+Vue.config.productionTip = false;
+App.mpType = "app";
+const app = new Vue({
+  ...App,
+});
+app.$mount();
+// #endif
+
+// #ifdef VUE3
+import {
+  createSSRApp
+} from "vue";
+export function createApp() {
+  const app = createSSRApp(App);
+  const pinia = Pinia.createPinia();
+  pinia.use(piniasistedstate);
+  app.use(pinia);
+  app.use(uviewPlus);
+  return {
+    app,
+    Pinia,
+  };
+}
+// #endif
