d81bca013c6210b7b1a212ff16d6ce35eade415e
fcjjjjjbd <162014680+fcjjjjjbd@users.noreply.github.com>
2025-11-09 04:48:54 +0800

    a

--- pages_fen/advpay/list.vue
+++ pages_fen/advpay/list.vue
@@ -20,7 +20,7 @@
         <view class="item" v-for="(item, index) in Paylist" :key="item._id">
           <adv-card
             :item="item"
-            @click="clickPicc(index)"
+            @clickPic="clickPicc(index)"
             @openpp="handleOpenComments"
           ></adv-card>
         </view>
@@ -131,7 +131,76 @@ onLoad((e) => {
 });
 
 const queryList = async (pageNo, pageSize) => {
-  
+  try {
+    const queryData = unref(query);
+    queryData.pageSize = Math.min(10, queryData.pageSize);
+    queryData.pageCurrent = (pageNo - 1) * queryData.pageSize;
+    let {
+      result: { errCode, data },
+    } = await db
+      .collection("pdd-adv")
+      .aggregate()
+      .match(`category_id=='${unref(query).category_id}' && order_status != 0 `)
+      .lookup({
+        from: "uni-id-users",
+        let: {
+          uid: "$user_id",
+        },
+        pipeline: $.pipeline()
+          .project({
+            username: 1,
+          })
+          .done(),
+        as: "userInfo",
+      })
+      .lookup({
+        from: "soup-like",
+        let: {
+          soupID: "$_id",
+        },
+        pipeline: $.pipeline()
+          .match(
+            dbCmd.expr(
+              $.and([
+                $.eq(["$like_type", 0]),
+                $.eq(["$$soupID", "$soup_id"]),
+                $.eq(["$user_id", current_id.value]),
+              ])
+            )
+          )
+          .count("length")
+          .done(),
+        as: "likeState",
+      })
+      .project({
+        isLike: $.cond({
+          if: $.gt([$.arrayElemAt(["$likeState.length", 0]), 0]),
+          then: true,
+          else: false,
+        }),
+        like_count: 1,
+        comment_count: 1,
+        content: 1,
+        imageValue: 1,
+        order_no: 1,
+        phone: 1,
+        order_status: 1,
+        userInfo: $.arrayElemAt(["$userInfo", 0]),
+      })
+      .sort({
+        like_count: -1,
+      })
+      .skip(queryData.pageCurrent)
+      .limit(queryData.pageSize)
+      .end();
+
+    if (errCode !== 0) return paging.value.complete(false);
+    paging.value.complete(data);
+
+    console.log(data);
+  } catch (err) {
+    paging.value.complete(false);
+  }
 };
 // 图片点击预览功能
 
